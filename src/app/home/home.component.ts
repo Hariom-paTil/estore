6177b5214fb0126c4d3b5912c66134a8f4b2d144
hariompatil <hariompatil757@gmail.com>
2025-01-23 20:37:57 +0530

    estore-9:creating 404 page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../header/header.component";
 import { CatnavigationbarComponent } from "../catnavigationbar/catnavigationbar.component";
+import { PagenotfoundComponent } from '../pagenotfound/pagenotfound.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CatnavigationbarComponent],
+  imports: [HeaderComponent, CatnavigationbarComponent,PagenotfoundComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
