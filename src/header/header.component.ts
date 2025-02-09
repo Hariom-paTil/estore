cceb6460163e3c5ad7aefe8913a68e44a8251a54
hariompatil <hariompatil757@gmail.com>
2025-02-09 13:13:15 +0530

    estore-28: populet header search bar

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faSearch,faUserCircle,faHeart,faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-
+import { CategoriesStoreItem } from '../app/services/categories.storeitem';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
@@ -14,4 +15,8 @@ faSearch = faSearch;
 faUserCircle= faUserCircle;
 faHeart= faHeart;
 faShoppingCart=faShoppingCart;
+
+constructor(public storeitem: CategoriesStoreItem){
+
+}
 }
